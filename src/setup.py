c09b944273331a380f3d9a8d54d558149d236973
Ehud <ehudhala@hotmail.com>
2016-09-02 09:17:37 +0300

    Added a setup.py

---
+++ src/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='class_organizer',
+    description='Organizes a list of children into classes, where every class has equal qualities of children.',
+    packages=find_packages(),
+    install_requires=[]
+)
